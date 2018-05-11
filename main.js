a1bc28a74f3834ff430df0690cdc8fb40a8eab1c
NicholasB85 <nicholas.w.burkhart@gmail.com>
2018-05-11 10:38:39 -0400

    added a color change animation

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
+// set of answers that can change.
 const answers = [
     'Maybe.', 'Outlook not so good', 'I really dont think so.',
     'There is a good chance.', 'Why are you asking me?',
     'do not ask me that', 'what do YOU think?','Yes.', 'Sure, why not.',
     'I guess anything is possible.',
 ]
-
+// set the onclick event and the function for the answers.
 document.getElementById('answerButton').onclick = function () {
     var answer = answers[Math.floor(Math.random() * answers.length)];
         document.getElementById('answerContainer').innerHTML = answer;
